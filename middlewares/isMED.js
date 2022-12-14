08901815a01a84ecf2ede0d6c3f98dfee9d9ff31
Joey Quadros <joey_qpb@hotmail.com>
2022-12-14 17:42:16 -0300

    fix ismed isroot ispac

--- middlewares/isMED.js
+++ middlewares/isMED.js
@@ -1,12 +1,7 @@
 export function isMED(req, res, next) {
-  try {
-    if (req.currentUser.role !== "MED") {
-      return res.status(401).json({ msg: "User Unauthorized" });
-    }
-
-    next();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
+  if (req.currentUser.role !== "MED") {
+    return res.status(401).json({ msg: "User Unauthorized" });
   }
+
+  next();
 }
--- middlewares/isROOT.js
+++ middlewares/isROOT.js
@@ -1,12 +1,7 @@
 export function isROOT(req, res, next) {
-  try {
-    if (req.currentUser.role !== "ROOT") {
-      return res.status(401).json({ msg: "User Unauthorized" });
-    }
-
-    next();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
+  if (req.currentUser.role !== "ROOT") {
+    return res.status(401).json({ msg: "User Unauthorized" });
   }
+
+  next();
 }
