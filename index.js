fa152a04687cfa36370d4ba57dca31add6a7259f
Joey Quadros <joey_qpb@hotmail.com>
2022-12-19 01:33:40 -0300

    fix deploy bug + init api doc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import { DBconnect } from "./config/db.config.js";
 
-import { rootRouter } from "./routes/root.routes.js";
+import { RootRouter } from "./routes/Root.routes.js";
 import { AGSRouter } from "./routes/AGS.routes.js";
 import { PACRouter } from "./routes/Pacientes.routes.js";
 import { MEDrouter } from "./routes/medico.routes.js";
@@ -10,6 +10,8 @@ import { MEDrouter } from "./routes/medico.routes.js";
 import { uploadImgRouter } from "./routes/uplouadImage.routes.js";
 
 import { recSenhaRouter } from "./routes/recSenha.routes.js";
+
+import { SwaggerRouter } from "./routes/swagger.routes.js";
 
 // configurar
 import cors from "cors";
@@ -25,7 +27,7 @@ app.use(express.json());
 
 const API_VERSION = "1.0";
 
-app.use(`/API/${API_VERSION}/Root`, rootRouter);
+app.use(`/API/${API_VERSION}/Root`, RootRouter);
 
 app.use(`/API/${API_VERSION}`, recSenhaRouter);
 
@@ -36,6 +38,8 @@ app.use(`/API/${API_VERSION}/PAC`, PACRouter);
 app.use(`/API/${API_VERSION}/MED`, MEDrouter);
 
 app.use(`/API/${API_VERSION}/upload_img`, uploadImgRouter);
+
+app.use(`/doc`, SwaggerRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up and running at DOOR: ${process.env.PORT}`);
